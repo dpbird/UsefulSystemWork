11b06d046e38c01983de9ca6787042692f09f9b9
Daniel Philip Bird <dpbird@andrew.cmu.edu>
2024-11-21 14:28:07 -0500

    Working on LatexInputRemover

--- Latex Input Remover/LatexInputRemover.py
+++ Latex Input Remover/LatexInputRemover.py
@@ -29,16 +29,21 @@ def splitFileAndPath(input):
     print(f"input to splitFileAndPath: {input}")
     file = ''
     loc = -1
-    while input[loc] != "\\" or input[loc] != "//":
+    while input[loc] != "\\" and input[loc] != "/":
         #TODO FIGURE OUT LOC GOING OUT OF RANGE!!!
+        # print(f"loc: {loc}, with input[loc]: {input[loc]}")
+        # print(input[loc] != "\\" or input[loc] != "/")
         file = input[loc] + file
         loc -= 1
+
+    
 
     return file, input[:loc]
 
 def genLatexFile(inputLatexPath, file):
+    initialPath = os.path.join(os.getcwd(), inputLatexPath)
     os.chdir(inputLatexPath)
-    print(f"CurrentDir: {os.getcwd()}")
+    # print(f"CurrentDir: {os.getcwd()}")
     latex = open(file).readlines()
     # print(latex)
     outputLatex = []
@@ -49,6 +54,8 @@ def genLatexFile(inputLatexPath, file):
             fileAdd, path = splitFileAndPath(inputContent)
 
             outputLatex.append(genLatexFile(path, fileAdd))
+            print(f"Initial Path: {initialPath}")
+            os.chdir(initialPath)
 
         else:
             outputLatex.append(line)
