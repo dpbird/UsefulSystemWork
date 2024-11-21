c360e21e526ed41e496d5cae3770aaf80df6ba8c
Daniel Philip Bird <dpbird@andrew.cmu.edu>
2024-11-21 14:09:52 -0500

    Updated files and added Latex Input remover

---
+++ Latex Input Remover/LatexInputRemover.py
@@ -0,0 +1,91 @@
+#Open a latex file and replace the \input{...} with the content of that input
+
+import os
+
+
+def findInputContent(s):
+    strLen = len(s)
+    currentPos = 0
+    generatedString = ""
+    for i in range(len(s)):
+        generatedString += s[i]
+        currentPos += 1
+        
+        if '\\input{' in generatedString:
+            
+            bracketPos = -1
+
+            while s[bracketPos] != '}':
+                bracketPos -=1
+            return s[currentPos:bracketPos]
+
+        
+            
+        
+    print("ERROR: '\\input{' not found in given string!")
+
+
+def splitFileAndPath(input):
+    print(f"input to splitFileAndPath: {input}")
+    file = ''
+    loc = -1
+    while input[loc] != "\\" or input[loc] != "//":
+        #TODO FIGURE OUT LOC GOING OUT OF RANGE!!!
+        file = input[loc] + file
+        loc -= 1
+
+    return file, input[:loc]
+
+def genLatexFile(inputLatexPath, file):
+    os.chdir(inputLatexPath)
+    print(f"CurrentDir: {os.getcwd()}")
+    latex = open(file).readlines()
+    # print(latex)
+    outputLatex = []
+    for line in latex:
+        if "\\input{" in line:
+            # print(line)
+            inputContent = findInputContent(line)
+            fileAdd, path = splitFileAndPath(inputContent)
+
+            outputLatex.append(genLatexFile(path, fileAdd))
+
+        else:
+            outputLatex.append(line)
+
+    return outputLatex
+
+
+
+
+
+
+if __name__ == "__main__":
+    inputLatexPath = "Latex\\quiz2\\"
+
+    # cwd = os.getcwd()
+
+    # os.chdir(os.path.join(cwd, inputLatexPath))
+
+    # print(f"cwd: {os.getcwd()}")
+    # os.chdir('../shared_quiz')
+
+    # print(f"cwd: {os.getcwd()}")
+    # # genLatexFile(inputLatexPath)
+    # file, path = splitFileAndPath('C:\\Users\\dpbird\\Documents\\Useful Systems\\Latex Input Remover\\Latex\\shared_quiz\\instruction.tex')
+    # print(f"file: {file}, path: {path}")
+    # for (root,dirs,files) in os.walk("Latex",topdown=True):
+    #       print("Directory path: %s"%root)
+    #       print("Directory Names: %s"%dirs)
+    #       print("Files Names: %s"%files)
+
+    # cwd = os.getcwd()
+    # print(cwd)
+    genLatexFile(inputLatexPath, 'main.tex')
+
+
+
+
+
+    # a = findInputContent("       \\input{Example.tex}")
+    # print(a)
